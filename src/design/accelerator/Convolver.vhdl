a45895214ee988b6b1b7ddec3952b173a39a1eb1
KateTruth <45755904+KateTruth@users.noreply.github.com>
2022-11-23 23:24:16 +0100

    Update Convolver.vhdl

--- src/design/accelerator/Convolver.vhdl
+++ src/design/accelerator/Convolver.vhdl
@@ -15,12 +15,24 @@ architecture Behavioral of Convolver is
 signal DXa: signed(7 downto 0);
 signal DXb: signed(7 downto 0);
 signal DXc: signed(7 downto 0);
+signal DXaext: signed(9 downto 0);
+signal DXbext: signed(9 downto 0);
+signal DXcext: signed(9 downto 0);
 signal DYa: signed(7 downto 0);
 signal DYb: signed(7 downto 0);
 signal DYc: signed(7 downto 0);
-signal DX: signed(7 downto 0);
-signal DY: signed(7 downto 0);
-
+signal DYaext: signed(9 downto 0);
+signal DYbext: signed(9 downto 0);
+signal DYcext: signed(9 downto 0);
+signal DX: signed(9 downto 0);
+signal DY: signed(9 downto 0);
+signal DXext: signed(10 downto 0);
+signal DYext: signed(10 downto 0);
+signal DXinv: signed(9 downto 0);
+signal DYinv: signed(9 downto 0);
+signal Dxabs: signed(9 downto 0);
+signal Dyabs: signed(9 downto 0);
+signal Dnabs: unsigned(10 downto 0);
 begin
 
     -- dummy connection
@@ -28,13 +40,34 @@ begin
     DXb <= signed(neighborhood(1,2)) - signed(neighborhood(1,0));
     DXc <= signed(neighborhood(2,2)) - signed(neighborhood(2,0));
     
+    Dxaext <= Dxa(7) & Dxa(7) & Dxa(7 downto 0);
+    Dxbext <= Dxb(7) & Dxb(7) & Dxb(7 downto 0);
+    Dxcext <= Dxc(7) & Dxc(7) & Dxc(7 downto 0);
+    
     DYa <= signed(neighborhood(0,0)) - signed(neighborhood(2,0)); 
     DYb <= signed(neighborhood(0,1)) - signed(neighborhood(2,1));
     DYc <= signed(neighborhood(0,2)) - signed(neighborhood(2,2));
     
-    DX <= DXa + (DXb sll 2) + DXc;
-    DY <= DYa + (DYb sll 2) + DYc;
-    pixel <= std_logic_vector(abs(DX) + abs(DY));
+    DYaext <= DYa(7) & DYa(7) & DYa(7 downto 0);
+    DYbext <= DYb(7) & DYb(7) & DYb(7 downto 0);
+    DYcext <= DYc(7) & DYc(7) & DYc(7 downto 0);
+    
+    DX <= DXaext + (DXbext sll 1) + DXcext;
+    DY <= DYaext + (DYbext sll 1) + DYcext;
+    
+    DXinv <= not(DX) + "0000000001";
+    DYinv <= not(DY) + "0000000001";
 
+    with DX(9) select
+         Dxabs <= DXinv when '1',
+                  Dx when others;
+    with DY(9) select
+         Dyabs <= DYinv when '1',
+                  DY when others;
 
+     Dxext <= Dxabs(9) & Dxabs(9 downto 0);
+     DYext <= DYabs(9) & DYabs(9 downto 0);
+     Dnabs <= unsigned(DXext + DYext);
+    pixel <= std_logic_vector(Dnabs(10 downto 3));
+    
 end architecture;
